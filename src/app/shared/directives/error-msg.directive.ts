413e66d476b32d94d095aa41497a186a3fa5af87
luisjavierluna <javierlunaluis@gmail.com>
2023-08-18 15:18:50 -0600

    8. Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -6,16 +6,22 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class ErrorMsgDirective implements OnInit{
   htmlElement: ElementRef<HTMLElement>
   @Input() color: string = 'red'
+  @Input() mensaje: string = 'Este campo es necesario'
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
   }
   ngOnInit(): void {
     this.setColor()
+    this.setMensaje()
   }
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color
+    this.htmlElement.nativeElement.classList.add('form-text')
   }
 
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerHTML = this.mensaje
+  }
 }
