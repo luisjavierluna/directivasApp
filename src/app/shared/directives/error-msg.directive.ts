551cb1260b17f345a7401fd92a508e39d0d76542
luisjavierluna <javierlunaluis@gmail.com>
2023-08-20 13:24:57 -0600

    10, 11, 12. Mostrar y Ocultar si tiene error el campo
    Se incluyó el contenido de las clases 10. Input setters y 11. Resolución del problema

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -4,9 +4,29 @@ import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit, OnChanges{
+
+  private _color: string = 'red'
+  private _mensaje: string = 'Este campo es requerido'
+
   htmlElement: ElementRef<HTMLElement>
-  @Input() color: string = 'red'
-  @Input() mensaje: string = 'Este campo es necesario'
+
+  @Input() set color(valor: string) {
+      this._color = valor
+      this.setColor()
+  }
+
+  @Input() set mensaje(valor: string) {
+    this._mensaje = valor
+    this.setMensaje()
+  }
+
+  @Input() set valido(valor: boolean) {
+    if(valor) {
+      this.htmlElement.nativeElement.classList.add('hidden')
+    } else {
+      this.htmlElement.nativeElement.classList.remove('hidden')
+    }
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
@@ -14,28 +34,32 @@ export class ErrorMsgDirective implements OnInit, OnChanges{
 
   ngOnChanges(changes: SimpleChanges): void {
 
-    if (changes['mensaje']) {
-      const mensaje = changes['mensaje'].currentValue
-      this.htmlElement.nativeElement.innerText = mensaje
-    }
+    // if (changes['mensaje']) {
+    //   const mensaje = changes['mensaje'].currentValue
+    //   this.htmlElement.nativeElement.innerText = mensaje
+    // }
 
-    if (changes['color']) {
-      const color = changes['color'].currentValue
-      this.htmlElement.nativeElement.style.color = color
-    }
+    // if (changes['color']) {
+    //   const color = changes['color'].currentValue
+    //   this.htmlElement.nativeElement.style.color = color
+    // }
   }
 
   ngOnInit(): void {
+    this.setEstilo()
     this.setColor()
     this.setMensaje()
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color
+  setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text')
   }
 
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this._color
+  }
+
   setMensaje(): void {
-    this.htmlElement.nativeElement.innerHTML = this.mensaje
+    this.htmlElement.nativeElement.innerHTML = this._mensaje
   }
 }
